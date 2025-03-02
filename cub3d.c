f225419e5e99948d490a68ab513410bfd910fda0
csouita <souita404@gmail.com>
2025-03-02 20:45:43 +0000

    minor changes

--- cub3d.c
+++ cub3d.c
@@ -6,23 +6,22 @@
 /*   By: csouita <csouita@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 03:49:04 by csouita           #+#    #+#             */
-/*   Updated: 2025/02/28 23:37:38 by csouita          ###   ########.fr       */
+/*   Updated: 2025/03/02 20:45:24 by csouita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	check_empty(char *line)
+int check_empty(char *line)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	if (!line)
 		return (0);
 	while (line[i])
 	{
-		if (line[i] != ' ' && line[i] != '\t' && line[i] != '\v'
-			&& line[i] != '\f' && line[i] != '\r' && line[i] != '\n')
+		if (line[i] != ' ' && line[i] != '\t' && line[i] != '\v' && line[i] != '\f' && line[i] != '\r' && line[i] != '\n')
 			return (1);
 		i++;
 	}
@@ -45,11 +44,9 @@ int last_line(t_data *data)
 	return (1);
 }
 
-
-
-int	count_split(char **split)
+int count_split(char **split)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (split[i])
@@ -57,13 +54,13 @@ int	count_split(char **split)
 	return (i);
 }
 
-int  handle_colors(char *line)
+int handle_colors(char *line)
 {
-	int i = 0 ;
+	int i = 0;
 	int count = 0;
-	while(line[i])
+	while (line[i])
 	{
-		if(line[i] == ',')
+		if (line[i] == ',')
 			count++;
 		i++;
 	}
@@ -72,9 +69,9 @@ int  handle_colors(char *line)
 		ft_putstr_fd("Error\nInvaliiiiiiid color\n", 2);
 		exit(1);
 	}
-	char **split = ft_split(line,',');
+	char **split = ft_split(line, ',');
 
-	if(count_split(split) != 3)
+	if (count_split(split) != 3)
 	{
 		ft_putstr_fd("Error\nInvalid colrrrrr\n", 2);
 		exit(1);
@@ -82,40 +79,36 @@ int  handle_colors(char *line)
 	int color = create_trgb(1, ft_atoi(split[0]), ft_atoi(split[1]), ft_atoi(split[2]));
 	if (color == -1)
 	{
-		ft_putstr_fd("Error\nInvalid color",2);
+		ft_putstr_fd("Error\nInvalid color", 2);
 		exit(1);
 	}
 	return color;
 }
 
-int	check_xpm(t_data *data)
+int check_xpm(t_data *data)
 {
-	int	len;
-	
-	len = ft_strlen(data->no) ;
-	if (data->no[len - 1] != 'm' || data->no[len - 2] != 'p' || data->no[len
-		- 3] != 'x' || data->no[len - 4] != '.')
-	{	
+	int len;
+
+	len = ft_strlen(data->no);
+	if (data->no[len - 1] != 'm' || data->no[len - 2] != 'p' || data->no[len - 3] != 'x' || data->no[len - 4] != '.')
+	{
 		ft_putstr_fd("Error\nInvalid file extension1\n", 2);
 		return (1);
 	}
 	len = ft_strlen(data->so);
-	if (data->so[len - 1] != 'm' || data->so[len - 2] != 'p' || data->so[len
-		- 3] != 'x' || data->so[len - 4] != '.')
+	if (data->so[len - 1] != 'm' || data->so[len - 2] != 'p' || data->so[len - 3] != 'x' || data->so[len - 4] != '.')
 	{
 		ft_putstr_fd("Error\nInvalid file extension2\n", 2);
 		return (1);
 	}
 	len = ft_strlen(data->we);
-	if (data->we[len - 1] != 'm' || data->we[len - 2] != 'p' || data->we[len
-		- 3] != 'x' || data->we[len - 4] != '.')
+	if (data->we[len - 1] != 'm' || data->we[len - 2] != 'p' || data->we[len - 3] != 'x' || data->we[len - 4] != '.')
 	{
 		ft_putstr_fd("Error\nInvalid file extension3\n", 2);
 		return (1);
 	}
 	len = ft_strlen(data->ea);
-	if (data->ea[len - 1] != 'm' || data->ea[len - 2] != 'p' || data->ea[len
-		- 3] != 'x' || data->ea[len - 4] != '.')
+	if (data->ea[len - 1] != 'm' || data->ea[len - 2] != 'p' || data->ea[len - 3] != 'x' || data->ea[len - 4] != '.')
 	{
 		ft_putstr_fd("Error\nInvalid file extension4\n", 2);
 		return (1);
@@ -123,34 +116,30 @@ int	check_xpm(t_data *data)
 	return (0);
 }
 
-int	check_whitespace(char *line)
+int check_whitespace(char *line)
 {
-	int	i;
+	int i;
 
 	i = 0;
-	while (line[i] == ' ' || line[i] == '\t' || line[i] == '\v'
-		|| line[i] == '\f' || line[i] == '\r')
+	while (line[i] == ' ' || line[i] == '\t' || line[i] == '\v' || line[i] == '\f' || line[i] == '\r')
 		i++;
 	return (i);
 }
 
-int	parse_element(t_data *data, int *i)
+int parse_element(t_data *data, int *i)
 {
-	if (ft_strncmp(data->map[*i], "NO", 2) == 0 || ft_strncmp(data->map[*i],
-			"SO", 2) == 0 || ft_strncmp(data->map[*i], "WE", 2) == 0
-		|| ft_strncmp(data->map[*i], "EA", 2) == 0 || ft_strncmp(data->map[*i],
-			"F", 1) == 0 || ft_strncmp(data->map[*i], "C", 1) == 0)
+	if (ft_strncmp(data->map[*i], "NO", 2) == 0 || ft_strncmp(data->map[*i], "SO", 2) == 0 || ft_strncmp(data->map[*i], "WE", 2) == 0 || ft_strncmp(data->map[*i], "EA", 2) == 0 || ft_strncmp(data->map[*i], "F", 1) == 0 || ft_strncmp(data->map[*i], "C", 1) == 0)
 	{
 		return (1);
 	}
 	return (0);
 }
 
-int	count_textures(t_data *data)
+int count_textures(t_data *data)
 {
-	int		count;
-	int		fd;
-	char	*line;
+	int count;
+	int fd;
+	char *line;
 
 	count = 0;
 	fd = open(data->file, O_RDONLY);
@@ -162,9 +151,7 @@ int	count_textures(t_data *data)
 			free(line);
 			line = get_next_line(fd);
 		}
-		if (ft_strncmp(line, "NO", 2) == 0 || ft_strncmp(line, "SO", 2) == 0
-			|| ft_strncmp(line, "WE", 2) == 0 || ft_strncmp(line, "EA", 2) == 0
-			|| ft_strncmp(line, "F", 1) == 0 || ft_strncmp(line, "C", 1) == 0)
+		if (ft_strncmp(line, "NO", 2) == 0 || ft_strncmp(line, "SO", 2) == 0 || ft_strncmp(line, "WE", 2) == 0 || ft_strncmp(line, "EA", 2) == 0 || ft_strncmp(line, "F", 1) == 0 || ft_strncmp(line, "C", 1) == 0)
 		{
 			count++;
 		}
@@ -175,11 +162,11 @@ int	count_textures(t_data *data)
 	return (count);
 }
 
-int	count_line(t_data *data)
+int count_line(t_data *data)
 {
-	int		fd;
-	char	*line;
-	int		line_count;
+	int fd;
+	char *line;
+	int line_count;
 
 	fd = open(data->file, O_RDONLY);
 	line = get_next_line(fd);
@@ -218,7 +205,7 @@ int check_player_valid_pos(t_data *data)
 			{
 				data->player_x = i;
 				data->player_y = j;
-				printf("data->map[i][j - 1] = %c\n", data->map[i - 1][j ]);
+				printf("data->map[i][j - 1] = %c\n", data->map[i - 1][j]);
 				if (one_or_zero(data->map[i + 1][j]) || one_or_zero(data->map[i - 1][j]) || one_or_zero(data->map[i][j + 1]) || one_or_zero(data->map[i][j - 1]))
 				{
 					ft_putstr_fd("Error\nInvalid player position\n", 2);
@@ -270,27 +257,30 @@ int check_player_valid_pos(t_data *data)
 // 	return 0;
 // }
 
-
-int count_len(t_data * data)
+int count_len(t_data *data)
 {
 	int i = 0;
 	int count = 0;
-	while(data->map[i])
+
+	if (!data->map)
+		return 0;
+
+	while (data->map[i])
 	{
-		if(check_empty(data->map[i]))
+		if (check_empty(data->map[i]))
 			count += ft_strlen(data->map[i]);
 		i++;
 	}
-	printf("count = %d\n", count);
+	// printf("count = %d\n", count);
 	return count;
 }
 
-int	parse_textures(t_data *data, int *i)
+int parse_textures(t_data *data, int *i)
 {
-	int		fd;
-	char	**split;
-	char	*line;
-	int		j;
+	int fd;
+	char **split;
+	char *line;
+	int j;
 
 	(void)i;
 	fd = open(data->file, O_RDONLY);
@@ -310,10 +300,10 @@ int	parse_textures(t_data *data, int *i)
 			free(line);
 			line = get_next_line(fd);
 			data->first_line_in_map++;
-			continue ;
+			continue;
 		}
 		if (j == 6)
-			break ;
+			break;
 		if (count_split(split) != 2)
 		{
 			printf("split[0] = %s\n", split[0]);
@@ -365,7 +355,6 @@ int	parse_textures(t_data *data, int *i)
 
 		line = get_next_line(fd);
 		j++;
-		
 	}
 	close(fd);
 	j = 0;
@@ -384,20 +373,19 @@ void first_line_in_map(t_data *data)
 			data->first_line_in_map++;
 			i++;
 		}
-		else 
+		else
 			i++;
 	}
 }
 
-int	is_valide2(char c)
+int is_valide2(char c)
 {
-	if (c == '0' || c == '1' || c == 'N' || c == 'S' || c == 'E' || c == 'W'
-		|| c == ' ' || c == '\n' || (c >= 9 && c <= 13))
+	if (c == '0' || c == '1' || c == 'N' || c == 'S' || c == 'E' || c == 'W' || c == ' ' || c == '\n' || (c >= 9 && c <= 13))
 		return (1);
 	return (0);
 }
 
-int	is_valide(char c)
+int is_valide(char c)
 {
 	if (c == '0' || c == '1' || c == 'N' || c == 'S' || c == 'E' || c == 'W')
 		return (1);
@@ -439,14 +427,14 @@ int first_and_last_lines_check(t_data *data)
 	return (0);
 }
 
-int	main(int ac, char *av[])
+int main(int ac, char *av[])
 {
-	int		i;
-	t_data	*data;
-	int		len;
-	int		fd;
-	char	*line;
-	int		j;
+	int i;
+	t_data *data;
+	int len;
+	int fd;
+	char *line;
+	int j;
 
 	i = 0;
 	data = malloc(sizeof(t_data));
@@ -459,20 +447,22 @@ int	main(int ac, char *av[])
 	if (ac != 2)
 	{
 		ft_putstr_fd("Error\nInvalid number of arguments\n", 2);
+		free(data);
 		return (1);
 	}
 	data->file = av[1];
 	len = ft_strlen(av[1]);
-	if (len < 4 || av[1][len - 1] != 'b' || av[1][len - 2] != 'u' || av[1][len
-		- 3] != 'c' || av[1][len - 4] != '.')
+	if (len < 4 || av[1][len - 1] != 'b' || av[1][len - 2] != 'u' || av[1][len - 3] != 'c' || av[1][len - 4] != '.')
 	{
 		ft_putstr_fd("Error\nInvalid path extension\n", 2);
+		free(data);
 		return (1);
 	}
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
 	{
 		ft_putstr_fd("Error\nInvalid file\n", 2);
+		free(data);
 		return (1);
 	}
 	line = get_next_line(fd);
@@ -501,14 +491,17 @@ int	main(int ac, char *av[])
 		free(line);
 		line = get_next_line(fd);
 	}
+	data->map[i] = NULL;
+	close(fd);
+
 	i = 0;
 	j = 0;
-	while (i < data->height)
+	while (i < data->height && data->map[i])
 	{
 		if (parse_element(data, &i))
 		{
 			i++;
-			continue ;
+			continue;
 		}
 		j = 0;
 		while (data->map[i][j])
@@ -526,37 +519,51 @@ int	main(int ac, char *av[])
 		}
 		i++;
 	}
+
 	i = 0;
 	j = 0;
-	// count_len(data);
-	while (i < data->height)
+	while (i < data->height && data->map[i])
 	{
 		if (parse_element(data, &i))
 		{
 			i++;
-			continue ;
+			continue;
 		}
 		j = 0;
-		
+
 		while (data->map[i][j])
 		{
-			if (data->map[i][j] == '0' && i > 0 && i < data->height - 1 && j > 0
-				&& j < (int)ft_strlen(data->map[i]) - 1)
+			if (data->map[i][j] == '0' && i > 0 && i < data->height - 1 && j > 0 && j < (int)ft_strlen(data->map[i]) - 1)
 			{
-				if (!check_empty(data->map[i + 1]) || !check_empty(data->map[i
-						- 1]) || !check_empty(data->map[i] + j + 1)
-					|| !check_empty(data->map[i] + j - 1))
+				if (i + 1 >= data->height || i - 1 < 0 || !data->map[i + 1] || !data->map[i - 1])
 				{
-					ft_putstr_fd("Error\nMap wwwis not closed\n", 2);
+					ft_putstr_fd("Error\nMap index out of bounds\n", 2);
 					exit(1);
 				}
-				if (i == 0 || i == data->height - 1 || j == 0
-					|| j == (int)ft_strlen(data->map[i]) - 1
-					|| !is_valide(data->map[i][j + 1])
-					|| !is_valide(data->map[i][j - 1]) || !is_valide(data->map[i
-						+ 1][j]) || !is_valide(data->map[i - 1][j]))
+
+				if (!check_empty(data->map[i + 1]) || !check_empty(data->map[i - 1]) ||
+					j + 1 >= (int)ft_strlen(data->map[i]) || j - 1 < 0 ||
+					!check_empty(data->map[i] + j + 1) || !check_empty(data->map[i] + j - 1))
 				{
-					ft_putstr_fd("Error\nMap is not closed\n", 2);
+					ft_putstr_fd("Error\nMap is not closed (emptiness check)\n", 2);
+					exit(1);
+				}
+
+				if (j + 1 >= (int)ft_strlen(data->map[i]) || j - 1 < 0 ||
+					i + 1 >= data->height || i - 1 < 0 ||
+					j >= (int)ft_strlen(data->map[i + 1]) || j >= (int)ft_strlen(data->map[i - 1]))
+				{
+					ft_putstr_fd("Error\nMap is not closed (boundary check)\n", 2);
+					for (int k = 0; k < data->height; k++)
+						free(data->map[k]);
+					free(data->map);
+					free(data);
+					return (1); 
+				}
+
+				if (i == 0 || i == data->height - 1 || j == 0 || j == (int)ft_strlen(data->map[i]) - 1 || !is_valide(data->map[i][j + 1]) || !is_valide(data->map[i][j - 1]) || !is_valide(data->map[i + 1][j]) || !is_valide(data->map[i - 1][j]))
+				{
+					ft_putstr_fd("Error\nMap is not closed (validation check)\n", 2);
 					for (int k = 0; k < data->height; k++)
 						free(data->map[k]);
 					free(data->map);
@@ -568,11 +575,10 @@ int	main(int ac, char *av[])
 		}
 		i++;
 	}
-	i = 0;
+
 	last_line(data);
-	// printf("last line in map = %d\n", data->last_line_in_map);
 	parse_textures(data, &i);
-	// check_valid_map(data);
+
 	if (check_xpm(data))
 	{
 		for (int k = 0; k < data->height; k++)
@@ -581,12 +587,10 @@ int	main(int ac, char *av[])
 		free(data);
 		return (1);
 	}
+
 	first_line_in_map(data);
-	// printf("dkhelt hna\n");
-	// exit(0);
-	// printf("first line in map = %d\n", data->first_line_in_map);
-	// printf("first and last lines check = %d\n", first_and_last_lines_check(data));
-	if(first_and_last_lines_check(data))
+
+	if (first_and_last_lines_check(data))
 	{
 		for (int k = 0; k < data->height; k++)
 			free(data->map[k]);
@@ -594,22 +598,9 @@ int	main(int ac, char *av[])
 		free(data);
 		return (1);
 	}
+
 	check_player_valid_pos(data);
-	// check_player_pos(data);
-	// if (check_player_pos(data))
-	// {
-	// 	for (int k = 0; k < data->height; k++)
-	// 		free(data->map[k]);
-	// 	free(data->map);
-	// 	free(data);
-	// 	return (1);
-	// }
-	i = 0;
-	// while(data->map[i])
-	// {
-	// 	printf("%s\n", data->map[i]);
-	// 	i++;
-	// }
+
 	printf("NO = %s\n", data->no);
 	printf("SO = %s\n", data->so);
 	printf("WE = %s\n", data->we);
@@ -624,5 +615,40 @@ int	main(int ac, char *av[])
 	printf("C_KEY = %s\n", data->c_key);
 	printf("color_f = %d\n", data->color_f);
 	printf("color_c = %d\n", data->color_c);
+
+	// Free allocated memory
+	for (int k = 0; k < data->height; k++)
+		free(data->map[k]);
+	free(data->map);
+
+	// Free texture paths
+	if (data->no)
+		free(data->no);
+	if (data->so)
+		free(data->so);
+	if (data->we)
+		free(data->we);
+	if (data->ea)
+		free(data->ea);
+	if (data->f)
+		free(data->f);
+	if (data->c)
+		free(data->c);
+
+	// Free texture keys
+	if (data->no_key)
+		free(data->no_key);
+	if (data->so_key)
+		free(data->so_key);
+	if (data->we_key)
+		free(data->we_key);
+	if (data->ea_key)
+		free(data->ea_key);
+	if (data->f_key)
+		free(data->f_key);
+	if (data->c_key)
+		free(data->c_key);
+
+	free(data);
 	return (0);
 }
