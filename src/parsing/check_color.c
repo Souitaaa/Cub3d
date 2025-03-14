61c46aae8147859f369b48038e57d20e4d391d65
csouita <souita404@gmail.com>
2025-03-14 00:56:36 +0000

    added a lot

---
+++ src/parsing/check_color.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_color.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csouita <csouita@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 21:53:32 by csouita           #+#    #+#             */
+/*   Updated: 2025/03/14 00:30:03 by csouita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	create_trgb(int t, int r, int g, int b)
+{
+	if (r == -1 || g == -1 || b == -1)
+		return (-1);
+	return (t << 24 | r << 16 | g << 8 | b);
+}
+
+void	free_elements(t_map *data)
+{
+	if(data->no)
+		free(data->no);
+	if(data->so)
+		free(data->so);
+	if(data->we)
+		free(data->we);
+	if(data->ea)
+		free(data->ea);
+	if(data->info->c)
+		free(data->info->c);
+	if(data->info->f)
+		free(data->info->f);
+}
+
+void	free_gnawi(char **split)
+{
+	int	i;
+
+	i = -1;
+	while (split[++i])
+		free(split[i]);
+	free(split);
+}
+
+
+int	handle_colors(char *line , t_map *data)
+{
+	int		i;
+	int		count;
+	char	**split;
+	int		color;
+
+	i = 0;
+	count = 0;
+	while (line[i])
+	{
+		if (line[i] == ',')
+			count++;
+		i++;
+	}
+	if (count != 2)
+		ft_error("Error\nInvalid color\n", data);
+	split = ft_split(line, ',');
+	if (count_split(split) != 3)
+		ft_error("Error\nInvalid color\n" , data);
+	color = create_trgb(1, ft_atoi00(split[0]), ft_atoi00(split[1]),
+			ft_atoi00(split[2]));
+	if (color == -1)
+		ft_error("Error\nInvalid color\n", data);
+	free_gnawi(split);
+	return (color);
+}
---
+++ src/parsing/check_textures.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_textures.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csouita <csouita@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 00:40:39 by csouita           #+#    #+#             */
+/*   Updated: 2025/03/14 00:35:49 by csouita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	check_valid_file(char *line, t_map *data)
+{
+	if ((!line && data->info->height == 0) || count_len(data) == 0)
+	{
+		ft_putstr_fd("Error\nInvalid file\n", 2);
+		free_elements(data);
+		free_memory(data);
+	}
+}
+
+void	validate_texture_format(char **split, t_map *data)
+{
+	if (count_split(split) != 2)
+	{
+		ft_putstr_fd("Error\nInvalid texture\n", 2);
+		free_elements(data);
+		free_memory(data);
+	}
+}
+
+void check_free(t_map *data , int flag)
+{
+	if (flag)
+	{
+		free_elements(data);
+		free_memory(data);
+	}
+}
+
+void	process_texture_line(t_map *data, char **split)
+{
+	validate_texture_format(split, data);
+	if (!strcmp(split[0], "NO"))
+		set_no_texture(data, split);
+	else if (!strcmp(split[0], "SO"))
+		set_so_texture(data, split);
+	else if (!strcmp(split[0], "WE"))
+		set_we_texture(data, split);
+	else if (!strcmp(split[0], "EA"))
+		set_ea_texture(data, split);
+	else if (!strcmp(split[0], "F"))
+		set_floor_and_ceiling_color(data, split, 1);
+	else if (!strcmp(split[0], "C"))
+		set_floor_and_ceiling_color(data, split, 0);
+}
+
+void	process_texture_data(t_map *data, char *line, int *j)
+{
+	char	**split;
+
+	split = ft_split00(line);
+	if (!check_empty(line))
+	{
+		free(line);
+		data->info->first_line_in_map++;
+		return ;
+	}
+	process_texture_line(data, split);
+	free(split);
+	free(line);
+	(*j)++;
+}
+
+int	parse_textures(t_map *data)
+{
+	int		fd;
+	char	*line;
+	int		j;
+
+	j = 0;
+	fd = open(data->info->file, O_RDONLY);
+	line = get_next_line(fd);
+	check_valid_file(line, data);
+	while (line && j < 6)
+	{
+		process_texture_data(data, line, &j);
+		line = get_next_line(fd);
+	}
+	check_valid_texture(data);
+	if (line)
+		free(line);
+	close(fd);
+	return (1);
+}
